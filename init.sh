c62264cadc666a5e699fb5c87425fd9062cc9880
Jonathan Lorimer <jonathan_lorimer@mac.com>
2022-03-20 00:39:12 -0400

    more shellcheck

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 set -e
 
-read -p "What do you want this haskell package to be called? " name
+read -r "What do you want this haskell package to be called? " name
 
 mv ./template.cabal "./$name.cabal"
 

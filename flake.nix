fe20ef5aa775793b063816bb3337fb52336f93b5
Jonathan Lorimer <jonathan_lorimer@mac.com>
2022-03-20 00:35:19 -0400

    fix templates in flake-utils

--- flake.nix
+++ flake.nix
@@ -15,32 +15,33 @@
     let utils = flake-utils.lib;
     in
     utils.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-    in
-    rec {
-      templates = {
-        haskell-flake = {
-          path = ./template;
-          description = "A template that for a haskell project that uses flakes";
-          welcomeText = ''
-            You just created a haskell flake project.
-            run this command to add your projects name:
-              nix run github:JonathanLorimer/haskell-flake-template
-          '';
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
+
+        packages.init =
+          pkgs.writeShellApplication {
+            name = "init";
+            text = (builtins.readFile ./init.sh);
+            runtimeInputs = with pkgs; [ ruplacer ];
+          };
+
+        apps.init = utils.mkApp { name = "init"; drv = packages.init; };
+        defaultApp = apps.init;
+      }) // {
+        templates = {
+          haskell-flake = {
+            path = ./template;
+            description = "A template that for a haskell project that uses flakes";
+            welcomeText = ''
+              You just created a haskell flake project.
+              run this command to add your projects name:
+                nix run github:JonathanLorimer/haskell-flake-template
+            '';
+          };
         };
+
+        defaultTemplate = self.templates.haskell-flake;
       };
-
-      defaultTemplate = templates.haskell-flake;
-
-      packages.init =
-        pkgs.writeShellApplication {
-          name = "init";
-          text = (builtins.readFile ./init.sh);
-          runtimeInputs = with pkgs; [ ruplacer ];
-        };
-
-      apps.init = utils.mkApp { name = "init"; drv = packages.init; };
-      defaultApp = apps.init;
-    });
 }

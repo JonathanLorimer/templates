590b72efae5ea44d0c6a47b0c022988b4be0829e
Jonathan Lorimer <jonathan_lorimer@mac.com>
2023-07-27 09:49:28 -0400

    move rust flake off flake utils

--- template/rust/flake.nix
+++ template/rust/flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/__nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
     crane.url = "github:ipetkov/crane";
     rustOverlay.url = "github:oxalica/rust-overlay";
     devshell.url = "github:numtide/devshell";
@@ -16,71 +15,81 @@
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
     crane,
     rustOverlay,
     devshell,
     advisory-db,
   }: let
-    utils = flake-utils.lib;
-  in
-    utils.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          (import rustOverlay)
-          devshell.overlays.default
-        ];
-      };
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ] (system: function rec {
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [
+                (import rustOverlay)
+                devshell.overlays.default
+              ];
+            };
 
-      rustToolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
-        toolchain.default.override {
-          extensions = ["rust-src"];
-        });
+            rustToolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
+              toolchain.default.override {
+                extensions = ["rust-src"];
+              });
 
-      craneLib = crane.lib.${system}.overrideToolchain rustToolchain;
+            craneLib = crane.lib.${system}.overrideToolchain rustToolchain;
 
-      src = craneLib.cleanCargoSource ./.;
+            src = craneLib.cleanCargoSource ./.;
 
-      craneCommon = {
-        inherit src;
-        RUSTFLAGS = [
-          # Lint groups
-          ["-D" "clippy::correctness"]
-          ["-D" "clippy::complexity"]
-          ["-D" "clippy::perf"]
-          ["-D" "clippy::style"]
-          ["-D" "clippy::nursery"]
-          ["-D" "clippy::pedantic"]
-          # Allowed by default
-          ["-D" "clippy::cognitive_complexity"]
-          ["-D" "clippy::expect_used"]
-          ["-D" "clippy::unwrap_used"]
-          ["-D" "clippy::print_stderr"]
-          ["-D" "clippy::print_stdout"]
-          ["-D" "clippy::pub_use"]
-          ["-D" "clippy::redundant_closure_for_method_calls"]
-          ["-D" "clippy::single_char_lifetime_names"]
-          ["-D" "clippy::str_to_string"]
-          ["-D" "clippy::string_to_string"]
-          ["-D" "clippy::unwrap_in_result"]
-          ["-D" "clippy::wildcard_enum_match_arm"]
-          # Allow certain rules
-          ["-A" "clippy::missing_errors_doc"]
-          ["-A" "clippy::module_name_repetitions"]
-          # No warnings
-          ["-D" "warnings"]
-        ];
-      };
+            craneCommon = {
+              inherit src;
+              RUSTFLAGS = [
+                # Lint groups
+                ["-D" "clippy::correctness"]
+                ["-D" "clippy::complexity"]
+                ["-D" "clippy::perf"]
+                ["-D" "clippy::style"]
+                ["-D" "clippy::nursery"]
+                ["-D" "clippy::pedantic"]
+                # Allowed by default
+                ["-D" "clippy::cognitive_complexity"]
+                ["-D" "clippy::expect_used"]
+                ["-D" "clippy::unwrap_used"]
+                ["-D" "clippy::print_stderr"]
+                ["-D" "clippy::print_stdout"]
+                ["-D" "clippy::pub_use"]
+                ["-D" "clippy::redundant_closure_for_method_calls"]
+                ["-D" "clippy::single_char_lifetime_names"]
+                ["-D" "clippy::str_to_string"]
+                ["-D" "clippy::string_to_string"]
+                ["-D" "clippy::unwrap_in_result"]
+                ["-D" "clippy::wildcard_enum_match_arm"]
+                # Allow certain rules
+                ["-A" "clippy::missing_errors_doc"]
+                ["-A" "clippy::module_name_repetitions"]
+                # No warnings
+                ["-D" "warnings"]
+              ];
+            };
 
-      cargoArtifacts = craneLib.buildDepsOnly craneCommon;
+            cargoArtifacts = craneLib.buildDepsOnly craneCommon;
 
-      __package_name = craneLib.buildPackage (craneCommon
-        // {
-          inherit cargoArtifacts;
-        });
+            __package_name = craneLib.buildPackage (craneCommon
+              // {
+                inherit cargoArtifacts;
+              });
+            });
     in {
-      checks = {
+      checks = forAllSystems ({
+        __package_name, 
+        craneLib, 
+        craneCommon, 
+        cargoArtifacts, 
+        src, ...
+      }: {
         inherit __package_name;
 
         __package_name-clippy = craneLib.cargoClippy (craneCommon
@@ -98,19 +107,30 @@
         __package_name-audit = craneLib.cargoAudit {
           inherit src advisory-db;
         };
-      };
+      });
 
-      formatter = pkgs.alejandra;
+      formatter = forAllSystems ({ pkgs, ... }: pkgs.alejandra);
 
-      packages.default = __package_name;
+      packages = forAllSystems ({ __package_name, ... }: { 
+        inherit __package_name;
+        default = __package_name; 
+      }); 
 
-      apps = {
-        default = utils.mkApp {
-          drv = __package_name;
+      apps = forAllSystems ({system, ...}: {
+        __package_name = { 
+          type = "app"; 
+          program = "${self.packages.${system}.__package_name}/bin/__package_name"; 
         };
-      };
+        default = self.apps.${system}.__package_name;
+      });
 
-      devShells.default = pkgs.devshell.mkShell {
+      devShells.default = forAllSystems ({
+        craneCommon, 
+        rustToolchain, 
+        pkgs, 
+        system, 
+        ...
+      }: {
         commands = let
           categories = {
             hygiene = "hygiene";
@@ -175,6 +195,6 @@
             eval = "\"${builtins.toString craneCommon.RUSTFLAGS}\"";
           }
         ];
-      };
-    });
+      });
+    };
 }

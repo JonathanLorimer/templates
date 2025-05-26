5dcc0ad7d616da31cb6482defd5b99dd49efd009
Jonathan Lorimer <jonathan_lorimer@mac.com>
2025-05-26 09:14:00 -0400

    feat(haskell): fix default test api deprecations

--- template/haskell/test/Main.hs
+++ template/haskell/test/Main.hs
@@ -4,19 +4,22 @@ import Data.Maybe (fromMaybe)
 import Spec as Spec
 import System.Environment (lookupEnv)
 import Test.Hspec (parallel)
+import Test.Hspec.Api.Formatters.V3 (specdoc, useFormatter)
 import Test.Hspec.Core.Runner (Config (..))
-import Test.Hspec.Formatters (specdoc)
 import Test.Hspec.Runner as TR (defaultConfig, hspecWith)
 import Text.Read (readMaybe)
 
 main :: IO ()
 main = do
   mText <- lookupEnv "TEST_CONCURRENCY"
-  let maxResources :: Int
-      maxResources = fromMaybe 8 (mText >>= readMaybe)
-  let cfg =
+  let
+    maxResources :: Int
+    maxResources = fromMaybe 8 (mText >>= readMaybe)
+  let
+    cfg =
+      useFormatter
+        ("specdoc", specdoc)
         TR.defaultConfig
           { configConcurrentJobs = Just maxResources
-          , configFormatter = Just specdoc
           }
   hspecWith cfg (parallel Spec.spec)
